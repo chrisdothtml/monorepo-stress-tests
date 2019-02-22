d2b0f6c1c75f4ce9d4ca410b65378902d2afcd52
Chris Deacy <deacy@uber.com>
2019-02-22 11:42:46 -0800

    deleting root node_modules before generating

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,5 +1,6 @@
 DIRNAME=$(dirname "$0")
 cd "$DIRNAME/../monorepo-root"
+rm -rf node_modules
 echo '{\n  "private": true\n}' > package.json
 cd packages
 "../../$DIRNAME/_generator.sh" generate ${1:-500}
