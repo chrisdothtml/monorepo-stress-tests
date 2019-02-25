ff19eeabbc6dd3959e56f56b457e598bad5bfe82
Chris Deacy <deacy@uber.com>
2019-02-25 12:00:06 -0800

    improving monorepo reset script

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,6 +1,13 @@
 DIRNAME=$(dirname "$0")
 cd "$DIRNAME/../monorepo-root"
-rm -rf node_modules
+
+# reset monorepo
+rm -rf *
+touch .gitkeep
+mkdir packages
+touch packages/.gitkeep
 echo '{\n  "private": true\n}' > package.json
+
+# generate benchmark packages
 cd packages
 "../../$DIRNAME/_generator.sh" generate ${1:-500}
